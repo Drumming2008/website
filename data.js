acf336e1c6c640b46406e479bf96e49589b9ac74
Drumming2008 <finn.reese@gmail.com>
2026-04-08 22:56:49 -0400

    font update

--- data.js
+++ data.js
@@ -49,6 +49,12 @@ let musicData = [
         video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/3S0DxOIJGFM?si=l9bTNMZujM8EW8Bk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
     },
     {
+        title: "Flow State",
+        instr: "Big Band",
+        year: 2026,
+        cat: "jazz"
+    },
+    {
         title: "For the Beauty of the Earth",
         instr: "TTBB Choir, <i>divisi</i>",
         year: 2025,
