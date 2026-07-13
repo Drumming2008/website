b3e6c4cf1b1e000ad1bb6fd2e1d477c132fbea83
Drumming2008 <finn.reese@gmail.com>
2026-07-13 16:44:15 -0400

    better videos

--- music.js
+++ music.js
@@ -1,43 +1,60 @@
 let musicPage = document.getElementById("music"),
-    musicCategoryElements = {}, musicSubcategoryElements = {}
+  musicCategoryElements = {}, musicSubcategoryElements = {}
+
+let videoID = 0
+for (let i of Object.keys(musicData)) {
+  musicData[i].videoID = videoID
+  // if (musicData[i].video) {
+  //   let videoWrapper = document.createElement("div")
+  //   videoWrapper.classList.add("plyr__video-embed")
+  //   videoWrapper.classList.add("video-inner-wrapper")
+  //   videoWrapper.id = `video-${musicData[i].videoID}`
+  //   videoWrapper.innerHTML = musicData[i].video
+  //   id("video-storage").append(videoWrapper)
+  // }
+
+  videoID++
+}
+
+
 
 for (let i of musicCategories) {
-    let details = document.createElement("details")
-    
-    let summary = document.createElement("summary")
-    summary.innerHTML = `
-        <h2>${i.name.replace("/", "<b>/</b>")}</h2>
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path></svg>
-    `
-    details.append(summary)
+  let details = document.createElement("details")
 
-    let content = document.createElement("ul")
-    content.classList.add("details-content")
-    details.append(content)
+  let summary = document.createElement("summary")
+  summary.innerHTML = `
+    <h2>${i.name.replace("/", "<b>/</b>")}</h2>
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path></svg>
+  `
+  details.append(summary)
 
-    id("music-content").append(details)
+  let content = document.createElement("ul")
+  content.classList.add("details-content")
+  details.append(content)
 
-    musicSubcategories.push({
-        name: "",
-        id: i.id + "-subcat",
-        parent: i.id
-    })
+  id("music-content").append(details)
 
-    for (let j of musicSubcategories) {
-        if (j.parent != i.id) continue
-        let wrapper = document.createElement("li")
-        let ul = document.createElement("ul")
-        ul.classList.add("sub-category")
-        let title = document.createElement("h3")
-        title.innerText = j.name
-        if (j.name) ul.append(title)
-        else ul.style.marginLeft = "-10px"
-        musicSubcategoryElements[j.id] = ul
-        wrapper.append(ul)
-        content.append(wrapper)
-    }
+  musicSubcategories.push({
+    name: "",
+    id: i.id + "-subcat",
+    parent: i.id
+  })
 
-    musicCategoryElements[i.id] = content
+  for (let j of musicSubcategories) {
+    if (j.parent != i.id) continue
+    let wrapper = document.createElement("li")
+    let ul = document.createElement("ul")
+    ul.classList.add("sub-category")
+    let title = document.createElement("h3")
+    title.innerText = j.name
+    if (j.name) ul.append(title)
+    else ul.style.marginLeft = "-10px"
+    musicSubcategoryElements[j.id] = ul
+    wrapper.append(ul)
+    content.append(wrapper)
+  }
+
+  musicCategoryElements[i.id] = content
 }
 
 function sortListAlphabetically(list) {
@@ -56,97 +73,118 @@ function sortListAlphabetically(list) {
 }
 
 for (let i of Object.values(musicData)) {
-    let piece = document.createElement("li")
+  let piece = document.createElement("li")
 
-    let title = document.createElement("div")
-    let titleLink = document.createElement("a")
-    titleLink.href = "/piece/" + (i.id || i.title.toLowerCase().replaceAll(" ", "-"))
-    titleLink.innerHTML = `${i.title}`
-    title.append(titleLink)
+  let title = document.createElement("div")
+  let titleLink = document.createElement("a")
+  titleLink.href = "/piece/" + (i.id || i.title.toLowerCase().replaceAll(" ", "-"))
+  titleLink.innerHTML = `${i.title}`
+  title.append(titleLink)
 
-    if (i.arrangement) {
-        let arrangement = document.createElement("span")
-        arrangement.innerText = "Arrangement"
-        arrangement.classList.add("arrangement-label")
-        title.prepend(arrangement)
-    }
+  if (i.arrangement) {
+    let arrangement = document.createElement("span")
+    arrangement.innerText = "Arrangement"
+    arrangement.classList.add("arrangement-label")
+    title.prepend(arrangement)
+  }
 
-    let titleInfo = document.createElement("div")
-    titleInfo.innerHTML = `<span>${i.composer ? " by " + i.composer : ""}</span><span> ${i.instr == "Lead Sheet" ? "" : "for "}${i.instr.replace("divisi", "<i>divisi</i>")}<span class="comma">,</span></span><span class="year"> ${i.year}</span>`
-    title.append(titleInfo)
+  let titleInfo = document.createElement("div")
+  titleInfo.innerHTML = `<span>${i.composer ? " by " + i.composer : ""}</span><span> ${i.instr == "Lead Sheet" ? "" : "for "}${i.instr.replace("divisi", "<i>divisi</i>")}<span class="comma">,</span></span><span class="year"> ${i.year}</span>`
+  title.append(titleInfo)
 
-    titleLink.onclick = e => {
-        e.preventDefault()
-        parent.pushState(titleLink.href)
-        moveToTab("music")
-    }
+  titleLink.onclick = e => {
+    e.preventDefault()
+    parent.pushState(titleLink.href)
+    moveToTab("music")
+  }
 
-    piece.append(title)
+  piece.append(title)
 
-    if (i.subcat) {
-        musicSubcategoryElements[i.subcat].append(piece)
-    } else {
-        musicSubcategoryElements[i.cat + "-subcat"].append(piece)
-    }
+  if (i.subcat) {
+    musicSubcategoryElements[i.subcat].append(piece)
+  } else {
+    musicSubcategoryElements[i.cat + "-subcat"].append(piece)
+  }
 }
 
 for (let i of document.querySelectorAll(".sub-category")) {
-    sortListAlphabetically(i)
+  sortListAlphabetically(i)
 }
 
 for (let i of document.querySelectorAll(".details-content")) {
-    id("search-content").append(i.cloneNode(true))
+  id("search-content").append(i.cloneNode(true))
 }
 
 for (let i of document.querySelectorAll("#search-content a")) {
-    i.onclick = e => {
-        e.preventDefault()
-        parent.pushState(i.href)
-        moveToTab("music")
-    }
+  i.onclick = e => {
+    e.preventDefault()
+    parent.pushState(i.href)
+    moveToTab("music")
+  }
 }
 
 function search() {
-    if (!id("search-music").value.trim() && !id("filters").value) {
-        id("music-content").style.display = ""
-        id("search-content").style.display = ""
-        return
+  if (!id("search-music").value.trim() && !id("filters").value) {
+    id("music-content").style.display = ""
+    id("search-content").style.display = ""
+    return
+  }
+
+  id("music-content").style.display = "none"
+  id("search-content").style.display = "flex"
+
+  for (let i of document.querySelectorAll("#search-content a")) {
+    i.parentElement.parentElement.style.display = "none"
+    i.parentElement.parentElement.parentElement.parentElement.classList.add("hidden")
+    i.closest(".details-content").classList.add("hidden")
+  }
+
+  let numResults = 0
+  for (let i of document.querySelectorAll("#search-content a")) {
+    let value = 0
+    if (i.innerText.toLowerCase().includes(id("search-music").value.toLowerCase()) || !id("search-music").value.trim()) {
+      value++
+    }
+    if (i.parentElement.querySelector("div").lastChild.innerText.trim() == id("filters").value || !id("filters").value) {
+      value++
     }
 
-    id("music-content").style.display = "none"
-    id("search-content").style.display = "flex"
-
-    for (let i of document.querySelectorAll("#search-content a")) {
-        i.parentElement.parentElement.style.display = "none"
-        i.parentElement.parentElement.parentElement.parentElement.classList.add("hidden")
-        i.closest(".details-content").classList.add("hidden")
+    if (value == 2) {
+      i.parentElement.parentElement.style.display = ""
+      i.parentElement.parentElement.parentElement.parentElement.classList.remove("hidden")
+      i.closest(".details-content").classList.remove("hidden")
+      numResults++
     }
+  }
 
-    let numResults = 0
-    for (let i of document.querySelectorAll("#search-content a")) {
-        let value = 0
-        if (i.innerText.toLowerCase().includes(id("search-music").value.toLowerCase()) || !id("search-music").value.trim()) {
-            value++
-        }
-        if (i.parentElement.querySelector("div").lastChild.innerText.trim() == id("filters").value || !id("filters").value) {
-            value++
-        }
-
-        if (value == 2) {
-            i.parentElement.parentElement.style.display = ""
-            i.parentElement.parentElement.parentElement.parentElement.classList.remove("hidden")
-            i.closest(".details-content").classList.remove("hidden")
-            numResults++
-        }
-    }
-
-    id("search-num").innerHTML = `${numResults} r<span>esult${numResults == 1 ? "" : "s"}</span>`
+  id("search-num").innerHTML = `${numResults} r<span>esult${numResults == 1 ? "" : "s"}</span>`
 }
 
 id("search-music").oninput = () => {
-    search()
+  search()
 }
 
 id("filters").onchange = () => {
-    search()
+  search()
+}
+
+id("music").onscroll = () => {
+  let musicControlsPos = id("music-controls").getBoundingClientRect()
+  id("music").classList.remove("bordered")
+  for (let i of document.querySelectorAll("details")) {
+    let pos = i.getBoundingClientRect()
+    i.getElementsByTagName("summary")[0].classList.remove("bordered-details")
+    if (pos.top <= musicControlsPos.bottom && pos.bottom > musicControlsPos.bottom) {
+      id("music").classList.add("bordered")
+      i.getElementsByTagName("summary")[0].classList.add("bordered-details")
+      let left = 0, right = 0
+      let top = pos.top - musicControlsPos.bottom
+      let br = parseInt(document.documentElement.style.getPropertyValue("--br").replace("px", "")) || 5
+      let bottom = (musicControlsPos.bottom + br + 1) - pos.bottom
+
+      let clipPath = `inset(${top}px ${right}px ${bottom}px ${left}px round var(--br))`
+      id("music").style.setProperty("--clip-path", clipPath)
+      break
+    }
+  }
 }
--- script.js
+++ script.js
@@ -256,7 +256,15 @@ function moveToTab(tab, onClick = false, pageLoad = false) {
     id("piece-header-text").innerText = `“${data.title}”`
 
     if (data.video) {
-      id("piece").innerHTML += `<div class="video-wrapper">${data.video}</div>`
+      // let player = new Plyr("")
+      console.log("data.videoID", data.videoID)
+      id("piece").innerHTML += `
+      <div class="video-wrapper">
+        <div class="plyr__video-embed" id="video-${data.videoID}">
+          ${data.video}
+        </div>
+      </div>`
+
     }
 
     if (data.desc) {
@@ -271,6 +279,63 @@ function moveToTab(tab, onClick = false, pageLoad = false) {
     }
 
     // keep at end
+
+    let player = new Plyr(`#video-${data.videoID}`, {
+        // Custom controls layout
+        controls: [
+          "play", 
+          "progress", 
+          "duration",
+          "current-time", 
+          "mute", 
+          "volume",
+          "fullscreen"
+        ],
+        keyboard: {
+          focused: true,
+          global: true
+        },
+        tooltips: {
+          controls: false,
+          seek: true
+        },
+        youtube: { 
+          controls: 0,
+          showinfo: 0,
+          rel: 0
+        },
+        hideControls: false
+      })
+
+      let playerSVGs = {
+        pressed: [
+          // pause
+          '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M216,48V208a16,16,0,0,1-16,16H160a16,16,0,0,1-16-16V48a16,16,0,0,1,16-16h40A16,16,0,0,1,216,48ZM96,32H56A16,16,0,0,0,40,48V208a16,16,0,0,0,16,16H96a16,16,0,0,0,16-16V48A16,16,0,0,0,96,32Z"></path></svg>',
+          // muted speaker
+          '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M213.92,210.62a8,8,0,1,1-11.84,10.76L160,175.09v48.6a8.29,8.29,0,0,1-3.91,7.18,8,8,0,0,1-9-.56l-65.55-51A4,4,0,0,1,80,176.18V87.09L42.08,45.38A8,8,0,1,1,53.92,34.62Zm-27.21-55.46a8,8,0,0,0,11.29-.7,40,40,0,0,0,0-52.88,8,8,0,1,0-12,10.57,24,24,0,0,1,0,31.72A8,8,0,0,0,186.71,155.16Zm40.92-80.49a8,8,0,1,0-11.92,10.66,64,64,0,0,1,0,85.34,8,8,0,1,0,11.92,10.66,80,80,0,0,0,0-106.66ZM153,119.87a4,4,0,0,0,7-2.7V32.25a8.27,8.27,0,0,0-2.88-6.4,8,8,0,0,0-10-.16L103.83,59.33a4,4,0,0,0-.5,5.85ZM60,80H32A16,16,0,0,0,16,96v64a16,16,0,0,0,16,16H60a4,4,0,0,0,4-4V84A4,4,0,0,0,60,80Z"></path></svg>',
+          // small screen
+          '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M152,96V48a8,8,0,0,1,16,0V88h40a8,8,0,0,1,0,16H160A8,8,0,0,1,152,96ZM96,152H48a8,8,0,0,0,0,16H88v40a8,8,0,0,0,16,0V160A8,8,0,0,0,96,152Zm112,0H160a8,8,0,0,0-8,8v48a8,8,0,0,0,16,0V168h40a8,8,0,0,0,0-16ZM96,40a8,8,0,0,0-8,8V88H48a8,8,0,0,0,0,16H96a8,8,0,0,0,8-8V48A8,8,0,0,0,96,40Z"></path></svg>'
+        ],
+        notPressed: [
+          // play
+          '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M240,128a15.74,15.74,0,0,1-7.6,13.51L88.32,229.65a16,16,0,0,1-16.2.3A15.86,15.86,0,0,1,64,216.13V39.87a15.86,15.86,0,0,1,8.12-13.82,16,16,0,0,1,16.2.3L232.4,114.49A15.74,15.74,0,0,1,240,128Z"></path></svg>',
+          // unmuted speaker
+          '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M160,32.25V223.69a8.29,8.29,0,0,1-3.91,7.18,8,8,0,0,1-9-.56l-65.57-51A4,4,0,0,1,80,176.16V79.84a4,4,0,0,1,1.55-3.15l65.57-51a8,8,0,0,1,10,.16A8.27,8.27,0,0,1,160,32.25ZM60,80H32A16,16,0,0,0,16,96v64a16,16,0,0,0,16,16H60a4,4,0,0,0,4-4V84A4,4,0,0,0,60,80Zm126.77,20.84a8,8,0,0,0-.72,11.3,24,24,0,0,1,0,31.72,8,8,0,1,0,12,10.58,40,40,0,0,0,0-52.88A8,8,0,0,0,186.74,100.84Zm40.89-26.17a8,8,0,1,0-11.92,10.66,64,64,0,0,1,0,85.34,8,8,0,1,0,11.92,10.66,80,80,0,0,0,0-106.66Z"></path></svg>',
+          // full screen
+          '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M216,48V88a8,8,0,0,1-16,0V56H168a8,8,0,0,1,0-16h40A8,8,0,0,1,216,48ZM88,200H56V168a8,8,0,0,0-16,0v40a8,8,0,0,0,8,8H88a8,8,0,0,0,0-16Zm120-40a8,8,0,0,0-8,8v32H168a8,8,0,0,0,0,16h40a8,8,0,0,0,8-8V168A8,8,0,0,0,208,160ZM88,40H48a8,8,0,0,0-8,8V88a8,8,0,0,0,16,0V56H88a8,8,0,0,0,0-16Z"></path></svg>'
+        ]
+      }
+
+      player.on("ready", (event) => {
+        let buttonList = [...document.querySelectorAll(".plyr__controls button")]
+        for (let i = 0; i < buttonList.length; i++) {
+          let elem = buttonList[i]
+          elem.querySelector(".icon--not-pressed").remove()
+          elem.innerHTML += playerSVGs.notPressed[i].replace("fill=\"#000000\"", "fill=\"currentColor\" class=\"icon--not-pressed\"")
+          elem.querySelector(".icon--pressed").remove()
+          elem.innerHTML += playerSVGs.pressed[i].replace("fill=\"#000000\"", "fill=\"currentColor\" class=\"icon--pressed\"")
+        }
+      })
     id("piece").querySelector(".back-to-pieces").onclick = () => {
       id("tab-music").click()
     }
